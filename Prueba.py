7a7c169e710f8d927251b7f1a7469779a442abc4
Juaquigm <jmorongo@gmail.com>
2019-09-23 12:28:43 +0200

    Prueba_Final

--- Prueba.py
+++ Prueba.py
@@ -2,3 +2,5 @@ print("Esto es una prueba para ver si funciona: ")
 input("Introduzca un numero: ")
 #Supongamos un ejemplo que esto es editado en la pagina web
 print("Esto es de la página web")
+#Esto esta hecho en el Visual Studio
+print("Esto es del editor visual Studio")
